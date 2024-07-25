65b8b90f4a6b20e1add87cfb470e1504f11ccd48
choplise <155413983+choplise@users.noreply.github.com>
2024-07-25 12:07:19 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log(11);
+const todo_list=[{
+    id:1,
+    text:'Learn Janpanese',
+    completed:false
+},
+{
+    id:2,
+    text:'Learn Javascript',
+    completed:false
+},
+{
+    id:3,
+    text:'Play violant',
+    completed:false
+}];
+
+console.log(todo_list);
+for(let i=0;i<todo_list.length;i++){
+    console.log(todo_list[i].text);
+}
